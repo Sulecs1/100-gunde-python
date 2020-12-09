b40172d5c2d4cd450f5bb01329bd8fa64e5d2abe
Cemal Cici <cicicemal@gmail.com>
2020-12-09 13:49:29 +0300

    6. gün eklendi

---
+++ gun-006/reeborg-s-world-01.py
@@ -0,0 +1,13 @@
+def turn_around():
+    turn_left()
+    turn_left()
+
+def turning_square():
+    turn_left()
+    move()
+    turn_around()
+
+turning_square()
+turning_square()
+turning_square()
+turning_square()
