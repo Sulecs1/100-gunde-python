82e6696eb1297b62c51a7086a151c2892db1723a
Cemal Cici <cicicemal@gmail.com>
2020-12-18 14:02:52 +0300

    12. gün tamamlandı

--- gun-012/project-012.py
+++ gun-012/project-012.py
@@ -23,4 +23,51 @@
 
 ##### Kodlama #####
 
+import random
 
+print("""
+ _____  _____   _______   _____ ___   _   _ ___  ________ _   _ _____ 
+/  ___|/ _ \ \ / /_   _| |_   _/ _ \ | | | ||  \/  |_   _| \ | |_   _|
+\ `--./ /_\ \ V /  | |     | |/ /_\ \| |_| || .  . | | | |  \| | | |  
+ `--. \  _  |\ /   | |     | ||  _  ||  _  || |\/| | | | | . ` | | |  
+/\__/ / | | || |  _| |_    | || | | || | | || |  | |_| |_| |\  |_| |_ 
+\____/\_| |_/\_/  \___/    \_/\_| |_/\_| |_/\_|  |_/\___/\_| \_/\___/ 
+
+                                                             
+""")
+
+print("Sayı Tahmini Oyunuma Hoş Geldin!")
+print("Bu oyundaki amaç 1-100 arasında tahmin edilen sayıyı bilmek!")
+
+olusan_sayi = random.randint(1, 100)
+
+zorluk_derecesi = input("Zorluk dereceni seç. 'kolay' veya 'zor': ").lower()
+
+if zorluk_derecesi == "kolay":
+    hak = 10
+elif zorluk_derecesi == "zor":
+    hak = 5
+else:
+    print("Hatalı değer girdiniz. Programı yeniden başlatın.")
+
+oyun_bitti_mi = False
+
+while not oyun_bitti_mi:
+    tahmin_edilen_sayi = int(input("Bir sayı giriniz: "))
+    if tahmin_edilen_sayi == olusan_sayi:
+        print("Doğru bildin, tebrikler!")
+        oyun_bitti_mi = True
+    elif tahmin_edilen_sayi < olusan_sayi:
+        hak -= 1
+        if hak != -1:
+            print(f"Çok Küçük.\nToplamda {hak+1} hakkın kaldı.")
+        elif hak == -1:
+            oyun_bitti_mi = True
+    else:
+        hak -= 1
+        if hak != -1:
+            print(f"Çok Büyük.\nToplamda {hak+1} hakkın kaldı.")
+        elif hak == -1:
+            oyun_bitti_mi = True
+
+print(f"Hakkın kalmadı, kaybettin. Doğru cevap: {olusan_sayi}")
