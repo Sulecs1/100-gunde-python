7f58c058cdbe4b230c7052cb6f2576d09d647c56
Cemal Cici <cicicemal@gmail.com>
2020-12-19 14:47:05 +0300

    14. gün oluşturuldu

---
+++ gun-014/day-014.py
@@ -0,0 +1,9 @@
+# Higher Lower oyunu
+# Oyunun oynanma amacını http://www.higherlowergame.com/ adresinden öğrenebilirsiniz.
+# Buradaki oyun mantığını Instagram takipçi sayısı üzerinden oluşturdum.
+# Bu bölüm de projeyi bakmadan geliştireceğim.
+
+#### Yapılacaklar ####
+
+# 1. Logoyu ekrana yazdır.
+# 2.
