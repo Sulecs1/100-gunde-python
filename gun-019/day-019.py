00fb80adeaf8cbce768bd6d470be192bd60740d9
Cemal Cici <cicicemal@gmail.com>
2021-01-03 14:13:36 +0300

    19. Gün Tamamlandı

---
+++ gun-019/day-019.py
@@ -0,0 +1,46 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+# def move_forwards():
+#     tim.forward(10)
+
+# screen.listen()
+# screen.onkey(key="space", fun=move_forwards)  # onkey metodu, bir higher order function özelliğine sahiptir.
+# screen.exitonclick()
+#
+# # Higher Order Function, girdisi fonksiyon olan fonksiyonlara denir.
+# # (Higher Order Functions --> Üst Seviye Fonksiyonlar)
+
+
+# Challenge: Make an Etch-A-Sketch
+
+# def ileri_git():
+#     tim.forward(10)
+#
+# def geri_git():
+#     tim.backward(10)
+#
+# def sola_don():
+#     yeni_donus = tim.heading() + 10
+#     tim.setheading(yeni_donus)
+#
+# def saga_don():
+#     yeni_donus = tim.heading() - 10
+#     tim.setheading(yeni_donus)
+#
+# def ekrani_temizle():
+#     tim.clear()
+#     tim.penup()
+#     tim.home()
+#     tim.pendown()
+#
+# screen.listen()
+# screen.onkey(ileri_git, "w")
+# screen.onkey(geri_git, "s")
+# screen.onkey(sola_don, "a")
+# screen.onkey(saga_don, "d")
+# screen.onkey(ekrani_temizle, "c")
+# screen.exitonclick()
+
