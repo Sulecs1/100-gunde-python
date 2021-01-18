9240c1a103131e4e1c79ba5348881dc5f6384453
Cemal Cici <cicicemal@gmail.com>
2021-01-18 10:02:40 +0300

    5. Gün klasörü güncellendi

--- gun-005/project-005.py
+++ gun-005/project-005.py
@@ -1,4 +1,9 @@
-#Password Generator Project
+# Password Generator Project
+# Burada amaç, tanımlanan harfler, sayılar ve sembolleri kullanarak rasgele parolalar oluşturmak.
+# Parola oluşturulurken kişilere oluşturmak istedikleri parolaların kaç harften, sembolden ve sayıdan oluşturmak istediğini sormak.
+# Program iki seviyeden oluşuyor.
+# 1. Seviye kolay bir seviye. Karışık sıra olmadan önce harfler, sonra semboller ve sonrasında da sayılar kullanılıyor. (Örnek: JduE&!91)
+# 2. Seviye biraz daha zor bir seviye. Hem karakterler rasgele hem de oluşturulma sıraları rasgele (Örnek: g^2jk8&P)
 
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
